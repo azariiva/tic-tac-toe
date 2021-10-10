f00d0222e74e7fba58a4a8b8130d6dc3036fd6c5
azariiva <zar.isk.rus@gmail.com>
2021-10-10 16:10:20 +0300

    changed game board to 2D array board is drawn dynamically by two loops calculateWinner is properly working only for 3x3 board

--- src/index.js
+++ src/index.js
@@ -21,11 +21,11 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-    renderSquare(i) {
+    renderSquare(i, j) {
         return (
             <Square
-                value={this.props.squares[i]}
-                onClick={() => this.props.onClick(i)}
+                value={this.props.squares[i][j]}
+                onClick={() => this.props.onClick(i, j)}
             />
         );
     }
@@ -33,21 +33,11 @@ class Board extends React.Component {
     render() {
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {this.props.squares.map((row, i) =>
+                    <div className="board-row">
+                        {row.map((_, j) => this.renderSquare(i, j))}
+                    </div>
+                )}
             </div>
         );
     }
@@ -58,7 +48,7 @@ class Game extends React.Component {
         super(props);
         this.state = {
             history: [{
-                squares: Array(9).fill(null),
+                squares: Array(3).fill(Array(3).fill(null)),
                 turn: null
             }],
             stepNumber: 0,
@@ -99,7 +89,7 @@ class Game extends React.Component {
                 <div className="game-board">
                     <Board
                         squares={current.squares}
-                        onClick={i => this.handleClick(i)}
+                        onClick={(i, j) => this.handleClick(i, j)}
                     />
                 </div>
                 <div className="game-info">
@@ -110,22 +100,22 @@ class Game extends React.Component {
         );
     }
 
-    handleClick(i) {
+    handleClick(i, j) {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
-        const squares = current.squares.slice();
+        const squares = current.squares.map(row => row.slice());
 
-        if (calculateWinner(squares) || squares[i]) {
+        if (calculateWinner(squares) || squares[i][j]) {
             return;
         }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        squares[i][j] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             history: history.concat([{
                 squares: squares,
                 description: {
-                    value: squares[i],
-                    row: Math.floor(i / 3),
-                    column: i % 3,
+                    value: squares[i][j],
+                    row: i,
+                    column: j,
                 }
             }]),
             stepNumber: history.length,
@@ -141,21 +131,74 @@ class Game extends React.Component {
     }
 }
 
+// this logic works only for square fields with corresponding winCondition
 function calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    for (let line of lines) {
-        const [a, b, c] = line
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
+    const winCondition = 3;
+
+    // check rows
+    for (let row of squares) {
+        const savedValue = row[0];
+        if (savedValue) {
+            let ctr = 0;
+            for (let elem of row) {
+                if (elem === savedValue) {
+                    ctr++;
+                } else {
+                    break;
+                }
+                if (ctr === winCondition) {
+                    return savedValue;
+                }
+            }
+        }
+    }
+
+    // check columns
+    for (let i = 0; i < squares[0].length; ++i) {
+        const savedValue = squares[0][i];
+        if (savedValue) {
+            let ctr = 0;
+            for (let j = 0; j < squares.length; ++j) {
+                const elem = squares[j][i];
+                if (elem === savedValue) {
+                    ctr++;
+                } else {
+                    break;
+                }
+                if (ctr === winCondition) {
+                    return savedValue;
+                }
+            }
+        }
+    }
+
+    // check diagonals
+    let savedValue = squares[0][0];
+    if (savedValue) {
+        let ctr = 0;
+        for (let i = 0; i < squares.length; ++i) {
+            if (squares[i][i] === savedValue) {
+                ctr++;
+            } else {
+                break;
+            }
+            if (ctr === winCondition) {
+                return savedValue;
+            }
+        }
+    }
+    savedValue = squares[0][squares.length - 1];
+    if (savedValue) {
+        let ctr = 0;
+        for (let i = squares.length - 1; i >= 0; i--) {
+            if (squares[squares.length - i - 1][i] === savedValue) {
+                ctr++;
+            } else {
+                break;
+            }
+            if (ctr === winCondition) {
+                return savedValue;
+            }
         }
     }
     return null;
